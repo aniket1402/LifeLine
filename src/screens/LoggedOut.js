ab102579352a8f64af1da39ae410995d4d27f8f2
Aniket Gupta <aniketgupta1495@gmail.com>
2020-07-16 16:25:04 +0530

    hooks

--- src/screens/LoggedOut.js
+++ src/screens/LoggedOut.js
@@ -1,85 +1,79 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
 import colors from '../styles/colors';
 import RoundedButton from '../components/buttons/RoundedButton';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { transparentHeaderStyle } from '../styles/navigation';
 import NavBarButton from '../components/buttons/NavBarButtons';
-// import Login from './Login';
 
-export default class LoggedOut extends Component {
-    static navigationOptions = ({ navigation }) => ({
-        headerRight: () => <NavBarButton handleButtonPress={() => navigation.navigate('Login')} location='right' color={colors.white} text="Log In"/>,
-        headerStyle: transparentHeaderStyle,
-        headerTintColor: colors.white
-    })
-
-    constructor(props){
-        super(props)
-        this.onCreateAccountPress = this.onCreateAccountPress.bind(this)
-    }
+const LoggedOut = (props) => {
     
-    onFacebookPress() {
+    onFacebookPress = () => {
         alert('Facebook button')
     }
-    onCreateAccountPress({navigation}) {
-        const {navigate} = this.props.navigation;
-        // alert('Create button')
-        navigate('Signup')
+
+    onCreateAccountPress = () => {
+        props.navigation.navigate('Signup')
     }
-    onMoreOptionsPress() {
+
+    onMoreOptionsPress = () => {
         alert('More options')
     }
-    render() {
-        return (
-            <View style={styles.wrapper}>
-                <View style={styles.welcomeWrapper}>
-                    <Image
-                        source={require('../img/car1.png')}
-                        style={styles.logo}    
-                    />
-                    <Text style={styles.welcomeText}>Welcome to LifeLine</Text>
-                    <RoundedButton
-                        text="Continue with Facebook"
-                        textColor={colors.green01}
-                        background={colors.white}
-                        icon={<Icon name="facebook" size={20} style={styles.facebookButtonIcon}/>}
-                        handleOnPress={this.onFacebookPress}
-                    />
-                    <RoundedButton
-                        text="Create Account"
-                        textColor={colors.white}
-                        handleOnPress={this.onCreateAccountPress}
-                    />
-                    <TouchableHighlight style={styles.moreOptionsButton} underlayColor={'transparent'} onPress={this.onMoreOptionsPress}>
-                        <Text style={styles.moreOptionsButtonText}>More Options</Text>
+
+    return (
+        <View style={styles.wrapper}>
+            <View style={styles.welcomeWrapper}>
+                <Image
+                    source={require('../img/car1.png')}
+                    style={styles.logo}    
+                />
+                <Text style={styles.welcomeText}>Welcome to LifeLine</Text>
+                <RoundedButton
+                    text="Continue with Facebook"
+                    textColor={colors.green01}
+                    background={colors.white}
+                    icon={<Icon name="facebook" size={20} style={styles.facebookButtonIcon}/>}
+                    handleOnPress={onFacebookPress}
+                />
+                <RoundedButton
+                    text="Create Account"
+                    textColor={colors.white}
+                    handleOnPress={onCreateAccountPress}
+                />
+                <TouchableHighlight style={styles.moreOptionsButton} underlayColor={'transparent'} onPress={onMoreOptionsPress}>
+                    <Text style={styles.moreOptionsButtonText}>More Options</Text>
+                </TouchableHighlight>
+                <View style={styles.termAndConditions}>
+                    <Text style={styles.termsText}>By tapping continue, Create Account or More</Text>
+                    <Text style={styles.termsText}> options, </Text>
+                    <Text style={styles.termsText}>I agree to LifeLine's </Text>
+                    <TouchableHighlight style={styles.linkButton}>
+                        <Text style={styles.termsText}>Terms of service</Text>
                     </TouchableHighlight>
-                    <View style={styles.termAndConditions}>
-                        <Text style={styles.termsText}>By tapping continue, Create Account or More</Text>
-                        <Text style={styles.termsText}> options, </Text>
-                        <Text style={styles.termsText}>I agree to LifeLine's </Text>
-                        <TouchableHighlight style={styles.linkButton}>
-                            <Text style={styles.termsText}>Terms of service</Text>
-                        </TouchableHighlight>
-                        <Text style={styles.termsText}>, </Text>
-                        <TouchableHighlight style={styles.linkButton}>
-                            <Text style={styles.termsText}>Payments Terms of service</Text>
-                        </TouchableHighlight>
-                        <Text style={styles.termsText}>, </Text>
-                        <TouchableHighlight style={styles.linkButton}>
-                            <Text style={styles.termsText}>Privacy Policy</Text>
-                        </TouchableHighlight>
-                        <Text style={styles.termsText}> and </Text>
-                        <TouchableHighlight style={styles.linkButton}>
-                            <Text style={styles.termsText}>Nondiscrimination Policy</Text>
-                        </TouchableHighlight>
-                        <Text style={styles.termsText}>.</Text>
-                    </View>
+                    <Text style={styles.termsText}>, </Text>
+                    <TouchableHighlight style={styles.linkButton}>
+                        <Text style={styles.termsText}>Payments Terms of service</Text>
+                    </TouchableHighlight>
+                    <Text style={styles.termsText}>, </Text>
+                    <TouchableHighlight style={styles.linkButton}>
+                        <Text style={styles.termsText}>Privacy Policy</Text>
+                    </TouchableHighlight>
+                    <Text style={styles.termsText}> and </Text>
+                    <TouchableHighlight style={styles.linkButton}>
+                        <Text style={styles.termsText}>Nondiscrimination Policy</Text>
+                    </TouchableHighlight>
+                    <Text style={styles.termsText}>.</Text>
                 </View>
             </View>
-        )
-    }
+        </View>
+    )    
 }
+
+LoggedOut['navigationOptions'] = (props) => ({
+    headerRight: () => <NavBarButton handleButtonPress={() => props.navigation.navigate('Login')} location='right' color={colors.white} text="Log In"/>,
+    headerStyle: transparentHeaderStyle,
+    headerTintColor: colors.white,
+})
 
 const styles = StyleSheet.create({
     wrapper: {
@@ -92,7 +86,6 @@ const styles = StyleSheet.create({
         display: 'flex',
         marginTop: 50,
         padding: 30,
-        // alignItems: 'center'
     },
     welcomeText: {
         fontSize: 30,
@@ -137,3 +130,5 @@ const styles = StyleSheet.create({
         borderBottomColor: colors.white,
     },
 });
+
+export default LoggedOut
